ea04aaa6f84b87a551880c3627bbb328c63625a6
Eric Harvey <planetside0@gmail.com>
2023-11-19 21:31:07 -0500

    Working Sample Quiz - Depends on Flask API Backend

--- src/App.js
+++ src/App.js
@@ -1,79 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const questions = [
-    {
-      questionText: "Who is Prime Minister of India?",
-      answerOptions: [
-        { answerText: "Vijay Rupani", isCorrect: false },
-        { answerText: "Manmohan singh", isCorrect: false },
-        { answerText: "Narendra Modi", isCorrect: true },
-        { answerText: "Deep Patel", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Who is CEO of Tata?",
-      answerOptions: [
-        { answerText: "Jeff Bezos", isCorrect: false },
-        { answerText: "Ratan Tata", isCorrect: true },
-        { answerText: "Mukesh Ambani", isCorrect: false },
-        { answerText: "Gautam Adani", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "who is richest person in the world?",
-      answerOptions: [
-        { answerText: "Jeff Bezos", isCorrect: false },
-        { answerText: "Elon Musk", isCorrect: true },
-        { answerText: "Mukesh Ambani", isCorrect: false },
-        { answerText: "Warren Buffett", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "how many countries in the world?",
-      answerOptions: [
-        { answerText: "120", isCorrect: false },
-        { answerText: "183", isCorrect: false },
-        { answerText: "170", isCorrect: false },
-        { answerText: "195", isCorrect: true },
-      ],
-    },
-  ];
-
+  const [questions, setQuestions] = useState(null);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
-  const handleAnswerButtonClick = (isCorrect) => {
-    if (isCorrect === true) {
-      setScore(score + 1);
-    }
 
-    const nextQuetions = currentQuestion + 1;
-    if (nextQuetions < questions.length) {
-      setCurrentQuestion(nextQuetions);
+  useEffect(() => {
+    fetch("/api/questions")
+      .then((response) => response.json())
+      .then((json) => setQuestions(json))
+      .catch((error) => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    console.log("score has been updated: ", score);
+    if (!showScore) return;
+    const launchId = window.launchId;
+    fetch("/api/score/" + launchId + "/" + score + "/", {
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .catch((error) => console.error(error));
+  }, [score]);
+
+  const handleAnswerButtonClick = async (isCorrect) => {
+    if (isCorrect === true) {
+      setScore((score) => score + 1);
+      console.log(score);
+    }
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
     } else {
       setShowScore(true);
+      console.log("setShowScore is true:" + score);
     }
   };
 
   return (
     <>
-        <div className="bg-white pt-10 pb-8 shadow-xl sm:mx-auto sm:rounded-lg sm:px-10 w-128">
-          <h1 className="mb-4 text-4xl font-extrabold">Quiz</h1>
-          {showScore ? (
-            <div>
-              You scored {score} out of {questions.length}
-            </div>
-          ) : (
-            <>
-              <p>
-                Question {currentQuestion + 1} of {questions.length}
-              </p>
-              <p>----</p>
-              <p className="mb-4">{questions[currentQuestion].questionText}</p>
+      <div className="bg-white pt-10 pb-8 shadow-xl sm:mx-auto sm:rounded-lg sm:px-10 w-128">
+        <h1 className="mb-4 text-4xl font-extrabold">Quiz</h1>
+        {showScore ? (
+          <div>
+            You scored {score} out of {questions.length}
+          </div>
+        ) : (
+          <>
+            <p>
+              Question {currentQuestion + 1} of {questions && questions.length}
+            </p>
+            <p>----</p>
+            <p className="mb-4">
+              {questions && questions[currentQuestion].questionText}
+            </p>
 
-              <div className="flex space-x-4">
-                {questions[currentQuestion].answerOptions.map(
+            <div className="flex space-x-4">
+              {questions &&
+                questions[currentQuestion].answerOptions.map(
                   (answerOptions, index) => (
                     <button
                       key={index}
@@ -86,10 +71,10 @@ function App() {
                     </button>
                   )
                 )}
-              </div>
-            </>
-          )}
-        </div>
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 }
